70a7e6f2097059afd192e12cd76946cbedf9f7a4
Phrompong <33851765+Phrompong@users.noreply.github.com>
2022-01-01 09:13:51 +0700

    edit

--- src/controllers/common.controller.ts
+++ src/controllers/common.controller.ts
@@ -4,48 +4,60 @@ import IState from "../interfaces/state.interface";
 import { TYPES } from "../types";
 
 let state: IState;
+// const data =
+//   "กรุณากรอกข้อมูลต่อไปนี้เพื่อสั่งซื้อสินค้า \r" +
+//   "ชื่อรุ่น:  \r" +
+//   "คุณสมบัติ:  \r" +
+//   "สี:  \r" +
+//   "จำนวน:  \r" +
+//   "(โอน.COD):  \r" +
+//   "จำนวนเงิน:  \r" +
+//   "ชื่อผู้สั่ง:  \r" +
+//   "ที่อยู่:  \r" +
+//   "เบอร์:  \r " +
+//   "แอดมิน:  \r";
 
 async function convertMessage(data: string): Promise<transaction> {
   state = container.get<IState>(TYPES.State);
   try {
     const model = data
-      .substring(data.search("ชื่อรุ่น") + 8, data.search("คุณสมบัติ"))
+      .substring(data.search("ชื่อรุ่น:") + 9, data.search("คุณสมบัติ:"))
       .trim();
 
     const property = data
-      .substring(data.search("คุณสมบัติ") + 9, data.search("สี"))
+      .substring(data.search("คุณสมบัติ:") + 10, data.search("สี:"))
       .trim();
 
     const color = data
-      .substring(data.search("สี") + 3, data.search("จำนวน"))
+      .substring(data.search("สี:") + 4, data.search("จำนวน:"))
       .trim();
 
     const num = data
-      .substring(data.search("จำนวน") + 5, data.search("(โอน.COD)"))
+      .substring(data.search("จำนวน:") + 6, data.search("(โอน.COD):"))
       .trim()
-      .split("(")[0];
+      .split("(:")[0];
 
     const cod = data
-      .substring(data.search("(โอน.COD)") + 8, data.search("จำนวนเงิน"))
+      .substring(data.search("(โอน.COD):") + 9, data.search("จำนวนเงิน:"))
       .trim();
 
     const total = data
-      .substring(data.search("จำนวนเงิน") + 9, data.search("ชื่อผู้สั่ง"))
+      .substring(data.search("จำนวนเงิน:") + 10, data.search("ชื่อผู้สั่ง:"))
       .trim();
 
     const name = data
-      .substring(data.search("ชื่อผู้สั่ง") + 12, data.search("ที่อยู่"))
+      .substring(data.search("ชื่อผู้สั่ง:") + 13, data.search("ที่อยู่:"))
       .trim();
 
     const address = data
-      .substring(data.search("ที่อยู่") + 7, data.search("เบอร์"))
+      .substring(data.search("ที่อยู่:") + 8, data.search("เบอร์:"))
       .trim();
 
     const tel = data
-      .substring(data.search("เบอร์") + 5, data.search("แอดมิน"))
+      .substring(data.search("เบอร์:") + 6, data.search("แอดมิน:"))
       .trim();
 
-    const admin = data.substring(data.search("แอดมิน") + 7);
+    const admin = data.substring(data.search("แอดมิน:") + 7);
 
     const response: transaction = {
       model,
