3c5795a804007f64e8f3aa15112deae84c6c783e
Phrompong <33851765+Phrompong@users.noreply.github.com>
2022-01-02 16:45:29 +0700

    inhome

--- src/routes/v1/facebook.route.ts
+++ src/routes/v1/facebook.route.ts
@@ -11,6 +11,7 @@ const { get } = require("lodash");
 const request = require("request");
 const state = container.get<IState>(TYPES.State);
 
+//#region Man giveup
 router.get("/manGiveup/webhook", async (req, res) => {
   // Parse the query params
   const mode = req.query["hub.mode"];
@@ -39,6 +40,49 @@ router.post("/manGiveup/webhook", async (req, res) => {
       const events = body && body.entry && body.entry[0];
 
       await facebookController.sendMessage(sender, message, page);
+      console.log(`+++++ end +++++`);
+      return res.sendStatus(200);
+    }
+
+    console.log(`+++++ end (err) +++++`);
+    return res.sendStatus(400);
+  } catch (error) {
+    const err = error as Error;
+    console.log(`[webhook] : ${err.message}`);
+    return;
+  }
+});
+//#endregion
+
+const verifyTokenInHome = "inHomeToken";
+router.get("/inHome/webhook", async (req, res) => {
+  // Parse the query params
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+  if (mode === "subscribe" && token === verifyTokenInHome) {
+    res.send(challenge);
+  } else {
+    state.logger.error("WEBHOOK_VERIFIED");
+    res.sendStatus(403);
+  }
+});
+
+// * verfify success
+router.post("/inHome/webhook", async (req, res) => {
+  try {
+    const page = "inHome";
+    let { body } = req;
+    console.log(`+++++ ${page} +++++`);
+    console.log(`[Man giveup] : ${body.entry[0].messaging[0].message.text}`);
+
+    const message = body.entry[0].messaging[0].message.text;
+    const sender = body.entry[0].messaging[0].sender.id;
+
+    if (body.object === "page") {
+      const events = body && body.entry && body.entry[0];
+
+      await facebookController.forVerify(events);
       console.log(`+++++ end +++++`);
       return res.sendStatus(200);
     }
