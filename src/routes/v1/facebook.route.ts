58ff06a67d1ffc6ce14505583de57139b9a4fcd1
Phrompong <33851765+Phrompong@users.noreply.github.com>
2022-01-03 03:40:25 +0700

    edit

--- src/routes/v1/facebook.route.ts
+++ src/routes/v1/facebook.route.ts
@@ -32,7 +32,7 @@ router.post("/manGiveup/webhook", async (req, res) => {
     let { body } = req;
 
     console.log(`++++++++++ ${page} ++++++++++`);
-    console.log(`headers : ${JSON.stringify(req.headers)}`);
+    console.log(`headers : ${JSON.stringify(req.params)}`);
     console.log(`Body request : ${JSON.stringify(body)}`);
     console.log(`[${page}] : ${body.entry[0].messaging[0].message.text}`);
 
